4c790718445b364b4075d81cecb11dd49dc1142b
Michel Abdad <michel_abdad@hotmail.com>
2020-02-04 16:22:06 +0100

    render api data in html

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,15 +1,36 @@
-const PUBLIC_KEY = "dbab711d9dd31157460665c92e6ff4e3"
-const PRIVATE_KEY = "b035ba44c02df1cad1216a27750d4dceb7be6255"
+'use strict';
 
-fetch('https://gateway.marvel.com/v1/public/comics', {
-    method: 'GET',
-    headers: {
-        'apikey': PRIVATE_KEY
-    }
-})
+const PUBLIC_KEY = "?apikey=dbab711d9dd31157460665c92e6ff4e3"
+const PRIVATE_KEY = "?apikey=b035ba44c02df1cad1216a27750d4dceb7be6255"
+const url = "https://gateway.marvel.com"
+
+const renderComics = (comics) => {
+
+    return document.getElementById("main").appendChild(comics)
+
+}
+
+
+fetch(url + '/v1/public/comics' + PUBLIC_KEY)
     .then((response) => {
+
         return response.json()
+
     })
-    .then((myJson) => {
-        console.log(myJson)
+    .then((res) => {
+
+        console.log(res.data.results)
+
+        const comics = res.data.results
+        const list = document.createElement('ul')
+
+        comics.forEach( comic => {
+
+            const listItem = document.createElement('li')
+            listItem.innerText = comic.title
+            list.appendChild(listItem)
+
+        })
+        renderComics(list)
     })
+
