607be4742d6fa642bd6ac107d10953a54a94b9fa
Michel Abdad <michel_abdad@hotmail.com>
2020-02-10 14:17:49 +0100

    get search to work and add routie

--- docs/js/app.js
+++ docs/js/app.js
@@ -4,14 +4,28 @@ const PUBLIC_KEY = "?apikey=dbab711d9dd31157460665c92e6ff4e3"
 const PRIVATE_KEY = "?apikey=b035ba44c02df1cad1216a27750d4dceb7be6255"
 const url = "https://gateway.marvel.com"
 
-const renderComics = (comics) => {
+const renderHTML = (comics) => {
 
     return document.getElementById("main").appendChild(comics)
 
 }
 
 const searchComic = () => {
+    let input, filter, comicGrid, gridItem, comicTitle, i, txtValue
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    comicGrid = document.getElementById("comic-layout");
+    gridItem = comicGrid.getElementsByClassName("comic-grid");
 
+    for (i = 0; i < gridItem.length; i++) {
+        comicTitle = gridItem[i].getElementsByClassName("comic-title")[0];
+        txtValue = comicTitle.textContent || comicTitle.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            gridItem[i].style.display = "";
+        } else {
+            gridItem[i].style.display = "none";
+        }
+    }
 }
 
 fetch(url + '/v1/public/comics' + PUBLIC_KEY)
@@ -26,7 +40,8 @@ fetch(url + '/v1/public/comics' + PUBLIC_KEY)
 
         const comics = res.data.results
         const comicContainer = document.createElement('div')
-        comicContainer.setAttribute('class', 'comic-layout')
+        // comicContainer.setAttribute('class', 'comic-layout')
+        comicContainer.setAttribute('id', 'comic-layout')
 
         comics.forEach( comic => {
 
@@ -52,6 +67,6 @@ fetch(url + '/v1/public/comics' + PUBLIC_KEY)
 
 
         })
-        renderComics(comicContainer)
+        renderHTML(comicContainer)
     })
 
